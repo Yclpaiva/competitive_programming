da16dad49b1448b72ad8df342eac42d0f3826a52
yclpaiva <yclpaiva@gmail.com>
2024-05-08 15:19:03 -0300

    Update python.py

--- c#/python.py
+++ c#/python.py
@@ -1,7 +1,22 @@
-palavra = input().split().sort()
-palavra2 = input().split().sort()
+palavra = input()
+palavra2 = input()
 
-if palavra == palavra2:
-    print('S')
+palavra = sorted(list(palavra))
+palavra2 = sorted(list(palavra2))
+
+print(palavra,palavra2)
+while "*" in palavra2:
+    palavra2.remove("*")
+
+if len(palavra2) < len(palavra):
+    counter = 0
+    for letra in palavra2:
+        if letra in palavra:
+            counter += 1
+    if counter == len(palavra2):
+        print('S')
 else:
-    print('N')
+    if palavra == palavra2:
+        print('S')
+    else:
+        print('N')
